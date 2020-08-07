0dc58225810de703cce7c544c3c211286b74ed13
Howard Kennedy <hkennedyvi@gmail.com>
2020-08-07 13:47:33 -0700

    dummy blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Howard',
+        'title': 'Posty',
+        'content': 'Firsty yeahhhh',
+        'date_posted': 'August 7, 2020'
+    },
+    {
+        'author': 'Ainslee',
+        'title': 'Posty Two',
+        'content': 'Secondy yeahhhh',
+        'date_posted': 'August 7, 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
