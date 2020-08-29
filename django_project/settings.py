749ea7c6822c99d0273507bc103351d2c141d7b6
Howard Kennedy <hkennedyvi@gmail.com>
2020-08-29 15:08:05 -0700

    heroku edits made in settings, env varaibles

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ca40n0xo-p+alcp$+k@0x1r18mv4=v5sm4e0(&i&feixrxfr6_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['i-bloggy.herokuapp.com']
 
 
 # Application definition
